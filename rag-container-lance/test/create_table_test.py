274578a1070869d5a6cd571a440ed24e8d17f2cf
zpyzl <zpyzl@qq.com>
2025-06-09 11:28:45 +0800

    t

--- rag-container-lance/test/create_table_test.py
+++ rag-container-lance/test/create_table_test.py
@@ -4,7 +4,7 @@ import requests
 import sys
 from config import test_ip
 
-params = {'db_path':'/usr/src/.lancedb','table_name':'t1'}
+params = {'db_path':'/usr/src/.lancedb','table_name':'new_docs2'}
 r = requests.get(f'http://{test_ip}:5003/create_table', params=params)
 print(r)
 if r:
---
+++ rag-container-lance/test/merge_query.py
@@ -0,0 +1,23 @@
+import sys
+
+import lancedb
+
+db = lancedb.connect("/usr/src/.lancedb")
+table = db.open_table(sys.argv[1])
+# table.create_fts_index("filename")
+
+table2 = db.open_table(sys.argv[2])
+
+
+results = (
+    table.search().to_list()
+    #table.search("a",fts_columns="filename")
+    # table.search([5.4, 9.5])
+    # .where("org_list like '%o1%' AND person_list like '%p1%' AND secret_level='s1'")
+    # .limit(20).to_list()
+)
+print(f"row count:{table.count_rows()}")
+for row in results:
+    row['vector'] = ''
+    print(row)
+#print([r['filename'] for r in results])
